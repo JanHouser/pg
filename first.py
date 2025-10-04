3a66c9212eca80228123bd9d5f19ec72acc565df
JanHouser <jan007jan007@seznam.cz>
2025-10-04 12:54:38 +0000

    úkol

--- first.py
+++ first.py
@@ -1,1 +1,12 @@
-print("Hello World")
+def sudy_nebo_lichy(cislo):
+    if cislo % 2 == 0:
+        print(f"Číslo {cislo} je sudé")
+    else:
+        print(f"Číslo {cislo} je liché")
+
+if __name__ == "__main__":
+    sudy_nebo_lichy(5)
+    sudy_nebo_lichy(1000000)
+
+    vstup = int(input("Zadej číslo: "))
+    sudy_nebo_lichy(vstup)        
