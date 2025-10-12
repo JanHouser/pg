48d8173b7273003d282116c200f0d69030225ef3
JanHouser <jan007jan007@seznam.cz>
2025-10-12 13:28:35 +0000

    hotovo

--- second.py
+++ second.py
@@ -1,7 +1,27 @@
 def cislo_text(cislo):
-    # funkce zkonvertuje cislo do jeho textove reprezentace
-    # napr: "25" -> "dvacet pět", omezte se na cisla od 0 do 100
-    return "dvacet pět"
+    cislo = int(cislo)
+
+
+    jednotky = ['nula', 'jedna', 'dva', 'tři', 'čtyři', 'pět', 'šest', 'sedm', 'osm', 'devět']
+    nact = ['deset', 'jedenáct', 'dvanáct', 'třináct', 'čtrnáct', 'patnáct', 'šestnáct', 'sedmnáct', 'osmnáct', 'devatenáct']
+    desitky = ["", "", 'dvacet', 'třicet', 'čtyřicet', 'padesát', 'šedesát', 'sedmdesát', 'osmdesát', 'devadesát']
+
+    if cislo < 10:
+        return jednotky[cislo]
+    elif 10 <= cislo < 20:
+        return nact[cislo - 10]
+    elif cislo < 100:
+        d = cislo // 10
+        j = cislo % 10
+        if j == 0:
+            return desitky[d]
+        else:
+            return desitky[d] + " " + jednotky[j]
+    elif cislo == 100:
+        return "sto"
+    else:
+        return "mimo rozsah"
+
 
 if __name__ == "__main__":
     cislo = input("Zadej číslo: ")
