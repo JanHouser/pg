09093f1faf27c736eebf3dc569b8436410c356c8
JanHouser <jan007jan007@seznam.cz>
2025-10-19 12:23:20 +0000

    hotovo

--- third.py
+++ third.py
@@ -1,31 +1,35 @@
 def je_prvocislo(cislo):
-    """
-    Funkce overi, zda zadane cislo je nebo neni prvocislo a vrati True nebo False
-
-    Prvocislo je takove cislo vetsi nez 1, ktere neni delitelne zadnym jinym cislem jenom 1 a samo sebou.
-
-    Napoveda jak otestova prvocislo:
-    Cislo 36 vznikne nasobenim:
-    1 * 36
-    2 * 18
-    3 * 12
-    4 * 9
-    6 * 6
-    9 * 4
-    12 * 3
-    18 * 2
-    36 * 1
-    Jak vidite v druhe polovine se dvojice opakuji, tzn. v tomto pripade staci overit delitelnost pouze do 6 (vcetne)
-    """
+   
+   if cislo <= 1:
     return False
-
-def vrat_prvocisla(maximum):
-    """
-    Funkce spocita vsechna prvocisla v rozsahu 1 az maximum a vrati je jako seznam.
-    """
-    return [2,3,5]
+   
+   if cislo == 2:
+    return True
+   
+   for i in range(2, cislo):
+      if cislo % i == 0:
+         return False
+   return True
 
 if __name__ == "__main__":
-    cislo = input("Zadej maximum: ")
+    cislo = int(input("Zadej cislo: "))
+    prvocisla = je_prvocislo(cislo)
+    print(prvocisla)
+
+def vrat_prvocisla(maximum):
+    result = []
+
+    aktualní = 0
+
+    while aktualní < maximum:
+     if je_prvocislo(aktualní):
+      result.append(aktualní)
+     aktualní += 1
+
+    return result
+
+if __name__ == "__main__":
+    cislo = int(input("Zadej maximum: "))
     prvocisla = vrat_prvocisla(cislo)
     print(prvocisla)
+
