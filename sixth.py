f664e0b5f8537f8078a4fd3e650665cc2a1230f7
JanHouser <jan007jan007@seznam.cz>
2025-11-17 14:00:52 +0000

    nedokončeno

--- sixth.py
+++ sixth.py
@@ -9,7 +9,27 @@ def download_url_and_get_all_hrefs(url):
     pokud ano, najdete ve stazenem obsahu stranky response.content vsechny vyskyty
     <a href="url">odkaz</a> a z nich nactete url, ktere vratite jako seznam pomoci return
     """
+
+    response = requests.get(url)
+    if response.status_code != 200:
+        raise Exception(f"chybný status kód: {response.status_code}")
+    html = response.text
+
     hrefs = []
+    start = 0
+    while True:
+        start_link = html.find('<a href="', start)
+        if start_link == -1:
+            break
+
+        start_quote = start_link + len('<a href="')
+
+        end_quote = html.find('"', start_quote)
+        if end_quote == -1:
+            break
+
+        link = html[start_quote:end_quote]
+        hrefs.append(link)
 
     return hrefs
 
@@ -17,7 +37,8 @@ def download_url_and_get_all_hrefs(url):
 if __name__ == "__main__":
     try:
         url = sys.argv[1]
-        download_url_and_get_all_hrefs(url)
-    # osetrete potencialni chyby pomoci vetve except
+        odkazy = download_url_and_get_all_hrefs(url)
+        for odkaz in odkazy:
+            print(f'"{odkaz}"')
     except Exception as e:
         print(f"Program skoncil chybou: {e}")
